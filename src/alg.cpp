beb10c741e0b1f06c1b024d189d0a9ab0d1678bc
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-03-17 16:07:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,46 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int k = 0;
+  while (k < len) {
+    int i = k + 1;
+    while (i < len) {
+      if ((arr[k] + arr[i]) == value) {
+        count++;
+      }
+      i++;
+    }
+    k++;
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int k = len - 1;
+  do {
+    k = k - 1;
+  } 
+  while (arr[k] > value);
+    int i = 0;
+    while (i < endl) {
+      int j = endl;
+        while (j > i) {
+          if (arr[j] + arr[i] == value) {
+            count++;
+          }
+          j--;
+        }
+        i++;
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int k = 0;
+  while (k < len) {
+    int k1 = value - arr[k];
+    count = count + cbinsearch(&arr[k + 1], len - k - 1, k1);
+    k++;
+  }
+  return count;
 }
