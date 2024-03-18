eb6b553011874858da2b1f6bfc3e56eb01c15760
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-03-18 11:50:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,32 @@
 // Copyright 2021 NNTU-CS
+int cbinsearch(int *arr, int size, int value) {
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int i = mid + 1;
+            while (i < size && arr[i] == value) {
+                count++;
+                i++;
+            }
+            i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
+                count++;
+                i--;
+            }
+            return count;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return 0;
+}
+
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   int k = 0;
@@ -15,6 +43,7 @@ int countPairs1(int *arr, int len, int value) {
   return count;
   return 0;
 }
+
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int k = len - 1;
@@ -37,16 +66,7 @@ int countPairs2(int *arr, int len, int value) {
   return count;
   return 0;
 }
-int cbinsearch(int *arr, int size, int value) {
-  int count = 0;
-  for (int i = 0; i < size; i++) {
-    if (arr[i] == value) {
-      count++;
-    }
-  }
-  return count;
-  return 0;
-}
+
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   int k = 0;
