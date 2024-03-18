7dd687355aec4ad45e0098aacccca61ac5a7af85
flapik19 <selivanovvlad3211@gmail.com>
2024-03-18 12:21:44 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int *arr, int len, int value)
 {
   int count = 0;
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < len-1; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
@@ -17,7 +17,7 @@ int countPairs1(int *arr, int len, int value)
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
-    for (int j = len; j != i; j--) {
+    for (int j = len; j != i + 1; j--) {
       if (arr[i] + arr[j]) {
         count++;
       }
