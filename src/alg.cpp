02acfac753012739b73460b498374c4afc13bf28
FomenkovaAnastasia <160059344+FomenkovaAnastasia@users.noreply.github.com>
2024-03-18 21:15:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,31 @@
 // Copyright 2021 NNTU-CS
+int cbinsearch(int *arr, int size, int value) {
+    int low = 0;
+    int high = size - 1;
+    int count = 0;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] < value) {
+            low = mid + 1;
+        } else if (arr[mid] > value) {
+            high = mid - 1;
+        } else {
+            count++;
+            int index = mid - 1;
+            mid++;
+            while (arr[mid] == value) {
+                count++;
+                mid++;
+            }
+            while (arr[index] == value) {
+                count++;
+                index--;
+            }
+            break;
+        }
+    } return count;
+}
+
 int countPairs1(int *arr, int len, int value) {
   return 0;
     int count = 0;
@@ -22,38 +49,6 @@ int countPairs2(int *arr, int len, int value) {
     len = j + 1;
     count += countPairs1(arr, len, value);
     return count;
-}
-
-int binarySearch(int* arr, int el, int left, int right) {
-  int count = 0;
-  int mid = 0;
-  int temp = 0;
-  bool flag = false;
-  int i;
-  i = left - 1;
-  while ((left <= right) && (flag != true)) {
-      mid = (left + right) / 2;
-      if (arr[mid] == el) {
-          flag = true;
-          temp = mid;
-          count++;
-      }
-      if (arr[mid] > el) {
-          right = mid - 1;
-      } else {
-          left = mid + 1;
-      }
-  }
-  while (arr[mid] == el) {
-      mid++;
-      if (arr[mid] == el) count++;
-  }
-  mid = temp - 1;
-  while ((arr[mid] == el) && (mid > i)) {
-      if (arr[mid] == el) count++;
-      mid--;
-  }
-  return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
