b955a5e21290fac145be174f97df3f5d6e147262
FomenkovaAnastasia <160059344+FomenkovaAnastasia@users.noreply.github.com>
2024-03-18 20:23:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,13 +14,13 @@ int countPairs1(int *arr, int len, int value) {
   return 0;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0;
+  int k2 = 0;
   std::sort(arr, arr + len);
   int left = 0;
   int right = len - 1;
   while (left < right) {
       if (arr[left] + arr[right] == value) {
-          count++;
+          k2++;
           left++;
           right--;
       } else if (arr[left] + arr[right] < value) {
@@ -32,9 +32,9 @@ int countPairs2(int *arr, int len, int value) {
   return 0;
 }
 int binarySearch(int* arr, int el, int left, int right) {
-  int count = 0;
+  int k3 = 0;
   int mid = 0;
-  temp = 0;
+  int temp = 0;
   bool flag = false;
   int i;
   i = left - 1;
@@ -43,7 +43,7 @@ int binarySearch(int* arr, int el, int left, int right) {
       if (arr[mid] == el) {
           flag = true;
           temp = mid;
-          count++;
+          k3++;
       }
       if (arr[mid] > el) {
           right = mid - 1;
@@ -57,17 +57,17 @@ int binarySearch(int* arr, int el, int left, int right) {
   }
   mid = temp - 1;
   while ((arr[mid] == el) && (mid > i)) {
-      if (arr[mid] == el) count++;
+      if (arr[mid] == el) k3++;
       mid--;
   }
-  return count;
+  return k3;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count = 0, i = 0, el;
+  int k3 = 0, i = 0, el;
   while (i < len) {
       el = value - arr[i];
-      count += binarySearch(arr, el, i + 1, len - 1);
+      k3 += binarySearch(arr, el, i + 1, len - 1);
       i++;
   }
-  return count;
+  return k3;
 }
