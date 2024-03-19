3db60b43c090f63725ad26842fbe4a1ac1375727
flapik19 <selivanovvlad3211@gmail.com>
2024-03-19 10:15:58 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,14 +13,35 @@ int countPairs1(int *arr, int len, int value) {
     return 0;
 }
 int countPairs2(int *arr, int len, int value) {
-
-}
-int countPairs3(int *arr, int len, int value) {
-  int count = 0;
+int count = 0;
   for (int i = 0; i < len/2; i++) {
     for (int j = len; j > len/2; j --) {
       if (arr[i] + arr[j] == value)
         count++;
+    }
+  }
+  if (count != 0)
+    return count;
+  else  
+    return 0;
+}
+int countPairs3(int *arr, int len, int value) {
+  int count = 0;
+  int key = 0;
+  int left = 0;
+  int right = len;
+  int mid = 0;
+  for (int i = 0; i < len; i ++) {
+    key = value - arr[i];
+    while (true) {
+      mid = (right + left) / 2;
+      if (arr[i] > mid) 
+        left = mid;
+      else if (arr[i] < mid)
+        right = mid;
+      else
+        count ++;
+        break;
     }
   }
   if (count != 0)
