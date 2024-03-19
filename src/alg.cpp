d094945b53611027491481b1fec5f35bc3148808
flapik19 <selivanovvlad3211@gmail.com>
2024-03-19 10:25:48 +0300

    .

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; i++) {
+  for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j ++) {
       if (arr[i] + arr[j] == value)
         count++;
@@ -14,8 +14,11 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
 int count = 0;
-  for (int i = 0; i < len/2; i++) {
-    for (int j = len; j > len/2; j --) {
+while (arr[len] > value) {
+  len--;
+}
+for (int i = 0; i < len; i++) {
+    for (int j = len - 1; j > i; j--) {
       if (arr[i] + arr[j] == value)
         count++;
     }
