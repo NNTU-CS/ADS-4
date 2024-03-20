c7e3a5b315b4ec851f5616596e8febcc94798e1a
MayaGerasimenko <160390629+MayaGerasimenko@users.noreply.github.com>
2024-03-20 19:25:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,47 +1,54 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
+  int cnt = 0;
   for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
-        count++;
+        cnt++;
       }
     }
   }
-  return count;
+  return cnt;
 }
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; ++i) {
-    for (int j = len-1; j > i; --j) {
+  int k = len - 1;
+  int cnt = 0;
+  for (int i = len; i > 0; --i) {
+    if (arr[i] > value) {
+      k = i;
+    }
+  }
+  for (int i = 0; i < k; ++i) {
+    for (int j = k; j > i; --j) {
       if (arr[i] + arr[j] == value) {
-        count++;
+        cnt++;
       }
     }
   }
-  return count;
+  return cnt;
 }
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; i++) {
-    if (i > 0 && arr[i] == arr[i - 1]) {
-      continue;
-    }
-    int left = i + 1, right = len - 1;
-    while (left <= right) {
-      int sum = arr[i] + arr[left];
-      if (sum == value) {
-        count++;
-        break; 
-      } 
-      else if (sum < value) {
-        left++;
-      } 
-      else {
-        right--;
+  int cnt = 0;
+  for (int i = 0; i < len - 1; i++) {
+    int right = len;
+    int left = i;
+    while (left < right - 1) {
+      int middle = (left + right) / 2;
+      if (arr[i] + arr[middle] == value) {
+        cnt++;
+        int j = middle + 1;
+        while ((arr[i] + arr[j] == value) && (j < right)) {
+          cnt++;
+          j++;
+        }
+      }
+      if (arr[i] + arr[middle] < value) {
+        left = middle;
+      } else {
+        right = middle;
       }
     }
-  } 
-  return count;
+  }
+  return cnt;
 }
