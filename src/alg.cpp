5629d4f4da90749b618743bcaabbaf681489c151
andsvee <160344384+andsvee@users.noreply.github.com>
2024-03-23 00:04:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,56 +1,67 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   return 0;
-    int summ = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            if (value == arr[i] + arr[j]) {
-                summ++;
-            }
-        }
+int countPairs1(int* arr, int len, int value) {
+  int k = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        k++;
+      }
     }
-    return summ;
+  }
+  return k;
 }
+
 int countPairs2(int *arr, int len, int value) {
   return 0;
-    int summ = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = len-1; j > i; --j) {
-            if (value == arr[i] + arr[j]) {
-                summ++;
-            }
+  int r = len - 1;
+  while (arr[r] > value) {
+    r--;
+  }
+  int k = 0;
+  for (int i = 0; i < r; i++) {
+    for (int j = r; j > i; j--) {
+      if ((arr[i] + arr[j] == value) && (arr[i] >= 0 && arr[j] >= 0)) {
+        k++;
+      } else if (arr[i] + arr[j] < value) {
+        break;
         }
     }
-    return summ;
+  }
+  return k;
 }
+
 int countPairs3(int *arr, int len, int value) {
   return 0;
-int countPairs3(int* arr, int len, int value) {
-    int result = 0;
-    for (int i = 0; i < len - 1; i++) {
-    int l_bound = i;
-    int r_bound = len;
-    while (l_bound < r_bound - 1) {
-        int middle = (r_bound + l_bound) / 2;
-        if (arr[i] + arr[middle] == value) {
-            result++;
-            int j = middle + 1;
-            while ((arr[i] + arr[j] == value) && (j < r_bound)) {
-                result++;
-                j++;
-            }
-            j = middle - 1;
-            while ((arr[i] + arr[j] == value) && (j > l_bound)) {
-                result++;
-                j--;
-            }
-            break;
-            } else if (arr[i] + arr[middle] > value) {
-                r_bound = middle;
-            } else {
-                l_bound = middle;
-            }
+  int k = 0;
+  int left;
+  int right, mid, sredn;
+  for (int i = 0; i < len; ++i) {
+    right = len - 1;
+    left = i + 1;
+    sredn = value - arr[i];
+    while (left <= right) {
+      mid = (left + right) / 2;
+      if (arr[mid] == sredn) {
+        k++;
+        int p = mid - 1;
+        while ((p > i) && (arr[p] == arr[mid])) {
+          k++;
+          p--;
+        }
+        int p2 = mid + 1;
+        while ((i < p2) && (arr[p2] == arr[mid])) {
+          k++;
+          p2++;
+        }
+        break;
+        } else if (arr[mid] > sredn) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
         }
     }
-    return result;
+  }
+  return k;
 }
