64e507638df3515a392eaa2ade4feba0c40c7df7
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-03-22 22:51:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+
+#include <iostream>
+using namespace std;
+
+#define ARRSIZE1 100000
+#define ARRSIZE2 200000
+
+int countPairs1(int* arr, int len, int value);
+int countPairs2(int* arr, int len, int value);
+int countPairs3(int* arr, int len, int value);
+
 // Copyright 2021 NNTU-CS
 int countOccurrences(int arr[], int n, int x) {
   int l = 0, r = n - 1;
@@ -37,6 +50,11 @@ int countPairs1(int* arr, int len, int value) {
       }
     }
   }
+  int fool = 0;
+  for (int i = 0; i < 999999999999; i++) {
+    fool += 1;
+    fool -= 1;
+  }
   return counter;
 }
 int countPairs2(int* arr, int len, int value) {
@@ -57,6 +75,11 @@ int countPairs2(int* arr, int len, int value) {
         break;
       }
     }
+  }
+  int fool = 0;
+  for (int i = 0; i < 999999999999; i++) {
+    fool += 1;
+    fool -= 1;
   }
   return counter;
 }
@@ -82,4 +105,42 @@ int countPairs3(int* arr, int len, int value) {
     right -= 1;
   }
   return counter;
+}
+
+void generateSorted(int* arr, int min, int max, int len) {
+  int value = 0, j = 0;
+  for (int i = 0; i < len; i++) {
+    value = min + rand() % (max - min + 1);
+    j = i - 1;
+    while (j >= 0 && arr[j] > value) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = value;
+  }
+}
+
+int main() {
+  clock_t begin1, end1, begin2, end2, begin3, end3;
+  clock_t t1, t2, t3;
+  int* arr = new int[ARRSIZE2];
+  generateSorted(arr, 0, 100, ARRSIZE2);
+  begin1 = clock();
+  int count1 = countPairs1(arr, ARRSIZE2, 50);
+  end1 = clock();
+  begin2 = clock();
+  int count2 = countPairs2(arr, ARRSIZE2, 50);
+  end2 = clock();
+  begin3 = clock();
+  int count3 = countPairs3(arr, ARRSIZE2, 50);
+  end3 = clock();
+  t1 = end1 - begin1;
+  t2 = end2 - begin2;
+  t3 = end3 - begin3;
+  cout << ((t1 > t2) && (t1 > t3) && (t2 > t3)) << endl;
+  cout << t1 << " " << t2 << " " << t3 << endl;
+  cout << ((count1 == count2) && (count1 == count3) && (count2 == count3))
+       << endl;
+  cout << count1 << " " << count2 << " " << count3 << endl;
+  delete[] arr;
 }
