83bcc94d09ed2df02df264588f149f95a4ab424f
flapik19 <selivanovvlad3211@gmail.com>
2024-03-23 14:14:30 +0300

    try to fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,40 +16,48 @@ int left = 0;
 int right = len - 1;
 while (arr[right] > value)
   right--;
-while (left < right) {
-  int sum = arr[left] + arr[right];
-  if (sum == value) {
-    count++;
-    right--;
-    left++;
+for (int i = 0; i < right; i++) { 
+  for (int j = right; i < j; j++) {
+    if(arr[i] + arr[j] == value) 
+      count++;
   }
-  else if (sum > value) {
-    right--;
-  }
-  else  
-    left++;
 }
 return count;
 }
 
+int cbinsearch(int *arr, int size, int value) {
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+        if (arr[middle] == value) {
+            count++;
+            int neighL = middle - 1;
+            while (neighL >= 0 && arr[neighL] == value) {
+                count++;
+                neighL--;
+            }
+            int iright = middle + 1;
+            while (iright < size && arr[iright] == value) {
+                count++;
+                iright++;
+            }
+            return count;
+        } else if (arr[middle] < value) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return 0;
+}
+
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-  int key = 0;
-  int left = 0;
-  int right = len;
-  int mid = 0;
-  for (int i = 0; i < len; i ++) {
-    key = value - arr[i];
-    while (true) {
-      mid = (right + left) / 2;
-      if (arr[i] > mid) 
-        left = mid;
-      else if (arr[i] < mid)
-        right = mid;
-      else
-        count ++;
-        break;
-    }
+  for (int i = 0; i < len; i++) {
+    int key = value - arr[i];
+    count += cbinsearch(&arr[i+1], len - i - 1, key);
   }
   return count;
 }
