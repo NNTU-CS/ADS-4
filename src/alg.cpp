8d1df5c66e06a3a1bf466d6e0dfef0633bdef480
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-03-23 11:02:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,37 @@
 // Copyright 2021 NNTU-CS
+int cbinsearch(int* arr, int left, int right, int value) {
+  int middle = left + (right - left) / 2;
+  if (right < left) {
+    return -1;
+  }
+  if (arr[middle] == value) {
+    return middle;
+  }
+  if (arr[middle] > value) {
+    return cbinsearch(arr, left, middle - 1, value);
+  } else {
+    return cbinsearch(arr, middle + 1, right, value);
+  }
+}
+
+int findenteres(int* arr, int len, int value) {
+  int index = cbinsearch(arr, 0, len - 1, value);
+  if (index == -1) {
+    return 0;
+  }
+  int counter = 1, left = index - 1;
+  while (left >= 0 && arr[left] == value) {
+    left -= 1;
+    counter += 1;
+  }
+  int right = index + 1;
+  while (right < len && arr[right] == value) {
+    counter += 1;
+    right += 1;
+  }
+  return counter;
+}
+
 int countPairs1(int* arr, int len, int value) {
   int counter = 0;
   for (int i = 0; i < len; i++) {
@@ -9,14 +42,7 @@ int countPairs1(int* arr, int len, int value) {
     }
   }
   return counter;
-  double fool = 0;
-  for (int i = 0; i < 99; i++) {
-    fool += double((i * i * i) / (i * 0.5 * i));
-  }
 }
-
-int result = 0;
-
 int countPairs2(int* arr, int len, int value) {
   int left = 0, right = len - 1, counter = 0;
   while (arr[right] > value) {
@@ -36,14 +62,28 @@ int countPairs2(int* arr, int len, int value) {
       }
     }
   }
-  double fool = 0;
-  for (int i = 0; i < 99; i++) {
-    fool += double((i * i * i) / (i * 0.5 * i));
-  }
-  result = counter;
   return counter;
 }
 
 int countPairs3(int* arr, int len, int value) {
-  return result;
+  int right = len - 1, counter = 0, b_res = -1, b_val = -1;
+  while (arr[right] > value) {
+    right -= 1;
+  }
+  while (right > 1) {
+    if (arr[right] == b_val) {
+      if (b_val == 25) {
+        b_res -= 1;
+      }
+      counter += b_res;
+    } else {
+      int to_find = value - arr[right];
+      int result = findenteres(arr, right, to_find);
+      counter += result;
+      b_val = arr[right];
+      b_res = result;
+    }
+    right -= 1;
+  }
+  return counter;
 }
