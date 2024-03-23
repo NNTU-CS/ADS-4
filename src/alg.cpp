e0ef9371ee93cc418bd53232acafb5cd979b0532
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-03-23 12:59:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -44,21 +44,13 @@ int countPairs1(int* arr, int len, int value) {
   return counter;
 }
 int countPairs2(int* arr, int len, int value) {
-  int left = 0, right = len - 1, counter = 0;
+  int right = len - 1, counter = 0;
   while (arr[right] > value) {
     --right;
-  }
-  int b_right = right;
-  while (true) {
-    if (arr[left] + arr[right] == value) {
-      ++counter;
-    }
-    right -= 1;
-    if (right == left) {
-      right = b_right;
-      ++left;
-      if (right == left) {
-        break;
+  for (int i = 0; i <= right; i++) {
+    for (int j = i + 1; j <= right; j++) {
+      if (arr[i] + arr[j] == value) {
+        ++counter;
       }
     }
   }
