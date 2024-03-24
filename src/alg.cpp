6619954ef90fcd8ad2587b2f5656673f1ceeeb52
BazhanMax <bazhanmax@gmail.com>
2024-03-24 23:48:29 +0300

    переделал векторы чтобы работало более быстро и может более точно

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,57 +5,71 @@
 #include <vector>
 
 
-int countPairs1(int *arr, int len, int value) {
-  int sum = 0, j = 0;
-  for (int i = 0; i < len; ++i) {
-    for (int j = i + 1; j < len; ++j) {
-      if (arr[i] + arr[j] == value) {
-        sum++;
-      }
+int countPairs1(int* arr, int len, int value) {
+    int sum = 0, j = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                sum++;
+            }
+        }
     }
-  }
-  return sum;
+    return sum;
 }
 
-int countPairs2(int *arr, int len, int value) {
-  int sum = 0;
-  for (int j = len - 1; j > 0; --j) {
-    if (arr[j] > value) {
-      continue;
+int countPairs2(int* arr, int len, int value) {
+    int sum = 0;
+    for (int j = len - 1; j > 0; --j) {
+        if (arr[j] > value) {
+            continue;
+        }
+        for (int i = 0; i < j; ++i) {
+            if (arr[j] + arr[i] == value) {
+                sum++;
+            }
+        }
     }
-    for (int i = 0; i < j; ++i) {
-      if (arr[j] + arr[i] == value) {
-        sum++;
-      }
-    }
-  }
-  return sum;
+    return sum;
 }
 
-int countPairs3(int *arr, int len, int value) {
-  int amount = 0;
-  for (int i = 0; i < len; i++) {
-    int prec = len / 2;
-    int point = prec;
+int countPairs3(int* arr, int len, int value) {
+    int amount = 0;
     std::vector<int> vectorarr(arr, arr + len);
-    int search = value - arr[i];
+    int vect_size = vectorarr.size();
+    for (int i = 0; i < vect_size; i++) {
+        int prec = vect_size / 2;
+        int point = prec;
 
-    for (int a = 0; a < (sqrt(len) + 1); a++) {
-      prec /= 2;
-      if (prec == 0) {
-        prec = 1;
-      }
-      int now_value = vectorarr.at(point);
-      if (now_value == search) {
-        amount++;
-        vectorarr.erase(begin(vectorarr) + point);
-        // std::cout << point << " " << now_value << std::endl;
-      } else if (now_value > search) {
-        point -= prec;
-      } else if (now_value < search) {
-        point += prec;
-      }
+        int search = value - arr[i];
+
+        for (int a = 0; a < (vect_size); a++) {
+            prec /= 2;
+            if (prec == 0) {
+                prec = 1;
+            }
+            int now_value = vectorarr.at(point);
+            // рассчет схожести и удаление
+            if (now_value == search) {
+                amount++;
+                vectorarr.erase(begin(vectorarr) + point);
+                vect_size --;
+                // std::cout << point << " " << now_value << std::endl;
+            }
+            
+            // рассчет точки
+            else if (now_value > search) {
+                point -= prec;
+                if (point < vect_size) {
+                    break;
+                }
+            }
+            else if (now_value < search) {
+                point += prec;
+                if (point > vect_size) {
+                    break;
+                }
+            }
+        }
     }
-  }
-  return amount / 2;
+    return amount;
 }
