56e0002665a169de76418730d37e5c6e4e527633
BazhanMax <bazhanmax@gmail.com>
2024-03-25 00:34:50 +0300

    авось

--- src/alg.cpp
+++ src/alg.cpp
@@ -63,17 +63,18 @@ int countPairs3(int* arr, int len, int value) {
                 point -= prec;
                 if (point < 0) {
                     point = 0;
-                    break;
+                    // break;
                 }
             }
             else if (now_value < search) {
                 point += prec;
                 if (point >= vect_size) {
-                    point = vect_size - 1;
-                    break;
+                    point = vect_size-1;
+                    // break;
                 }
             }
         }
     }
     return amount;
 }
+
