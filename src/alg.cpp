4262dacf2a22c64b86721f93d7288d1b13a761b9
KorobovaKate <kat3kor@yandex.ru>
2025-03-23 19:18:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -41,30 +41,32 @@ int countPairs2(int *arr, int len, int value) {
 // cppcheck-suppress constParameterPointer
 int countPairs3(int *arr, int len, int value) {
   int kol = 0;
-  for (int i = 0; i < len; i++) {
-    int vtor = value - arr[i];
-    int left = i + 1;
-    int right = len - 1;
-    while (left <= right) {
-      int middle = left + (right - left) / 2;
-      if (arr[middle] == vtor) {
-        kol++;
-        int povt = middle - 1;
-        while (povt >= left && arr[povt] == vtor) {
-          kol++;
-          povt--;
-        }
-        povt = middle + 1;
-        while (povt <= right && arr[povt] == vtor) {
-          kol++;
-          povt++;
-        }
+  int left = 0;
+  int right = len - 1;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      if (arr[left] == arr[right]) {
+        int count = right - left + 1;
+        kol += count * (count - 1) / 2; 
         break;
-      } else if (arr[middle] < vtor) {
-        left = middle + 1;
-      } else {
-        right = middle - 1;
       }
+      int leftCount = 1, rightCount = 1;
+      while (left + 1 < right && arr[left] == arr[left + 1]) {
+        leftCount++;
+        left++;
+      }
+      while (left < right - 1 && arr[right] == arr[right - 1]) {
+        rightCount++;
+        right--;
+      }
+      kol += leftCount * rightCount;
+      left++;
+      right--;
+    } else if (sum < value) {
+      left++;
+    } else {
+      right--;
     }
   }
   return kol;
