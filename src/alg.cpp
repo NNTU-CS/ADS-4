671086024ffa78ce0316023ba0933791facff6e2
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-03-23 12:00:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
+#include <iostream>
+int countPairs1(const int *arr, int len, int value) {
   int count = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
@@ -10,13 +11,12 @@ int countPairs1(int *arr, int len, int value) {
     }
     return count;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(const int *arr, int len, int value) {
     int count = 0;
     int left = 0;
     int right = len - 1;
     while (left < right) {
         int sum = arr[left] + arr[right];
-
         if (sum == value) {
             count++;
             left++;
@@ -29,7 +29,7 @@ int countPairs2(int *arr, int len, int value) {
     }
     return count;
 }
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(const int *arr, int len, int value) {
   int count = 0;
     for (int i = 0; i < len; ++i) {
         int complement = value - arr[i];
