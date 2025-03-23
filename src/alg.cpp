9098cba4772cf8de9982a6626d61a796de97d408
KorobovaKate <kat3kor@yandex.ru>
2025-03-23 19:32:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,26 +26,6 @@ int countPairs2(int *arr, int len, int value) {
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      kol++;
-      left++;
-      right--;
-    } else if (sum < value) {
-      left++;
-    } else {
-      right--;
-    }
-  }
-  return kol;
-}
-
-// cppcheck-suppress constParameterPointer
-int countPairs3(int *arr, int len, int value) {
-  int kol = 0;
-  int left = 0;
-  int right = len - 1;
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
       if (arr[left] == arr[right]) {
         int count = right - left + 1;
         kol += count * (count - 1) / 2;
@@ -67,6 +47,39 @@ int countPairs3(int *arr, int len, int value) {
       left++;
     } else {
       right--;
+    }
+  }
+  return kol;
+}
+
+// cppcheck-suppress constParameterPointer
+int countPairs3(int *arr, int len, int value) {
+  int kol = 0;
+  for (int i = 0; i < len; i++) {
+    if (i > 0 && arr[i] == arr[i - 1]) {
+      continue;
+    }
+    int vtor = value - arr[i];
+    int left = i + 1;
+    int right = len - 1;
+    while (left <= right) {
+      int middle = left + (right - left) / 2;
+      if (arr[middle] == vtor) {
+        kol++;
+        int temp = middle - 1;
+        while (temp >= left && arr[temp] == vtor) {
+          temp--;
+        }
+        temp = middle + 1;
+        while (temp <= right && arr[temp] == vtor) {
+          temp++;
+        }
+        break;
+      } else if (arr[middle] < vtor) {
+        left = middle + 1;
+      } else {
+        right = middle - 1;
+      }
     }
   }
   return kol;
