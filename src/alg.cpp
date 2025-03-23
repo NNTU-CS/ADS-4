93acd942d0771d203d02c0453df0641699a3611b
KorobovaKate <kat3kor@yandex.ru>
2025-03-23 18:33:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(const int *arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
   int kol = 0;
   for (int i = 0; i < len; i++) {
     for (int j = i + 1; j < len; j++) {
@@ -11,7 +11,7 @@ int countPairs1(const int *arr, int len, int value) {
   return kol;
 }
 
-int countPairs2(const int *arr, int len, int value) {
+int countPairs2(int *arr, int len, int value) {
   int kol = 0;
   int left = 0;
   int right = len - 1;
@@ -30,7 +30,7 @@ int countPairs2(const int *arr, int len, int value) {
   return kol;
 }
 
-int countPairs3(const int *arr, int len, int value) {
+int countPairs3(int *arr, int len, int value) {
   int kol = 0;
   for (int i = 0; i < len; i++) {
     int vtor = value - arr[i];
