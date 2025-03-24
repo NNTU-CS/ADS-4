249612b19669204e8062240829d5c9bb6cc36978
Eternity8c <morozkind6@gmail.com>
2025-03-24 22:18:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,16 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-	int col = 0;
-	for (int i = 0; i < len-1; i++) {
-		for (int j = i + 1; j < len; j++) {
-			if (arr[i] + arr[j] == value) {
-				col++;
-			}
+int countPairs1(const int *arr, int len, int value) {
+    int col = 0;
+    for (int i = 0; i < len-1; i++) {
+	for (int j = i + 1; j < len; j++) {
+		if (arr[i] + arr[j] == value) {
+			col++;
 		}
 	}
-	return col;
+    }
+    return col;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(const int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
         for (int j = len - 1; j > i; j--) {
@@ -22,7 +22,7 @@ int countPairs2(int *arr, int len, int value) {
     }
     return count;
 }
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(const int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
         int other = value - arr[i];
