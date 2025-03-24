3d677ee69f071141b09037644b318fe3767a2746
Van4e7 <ivanzvectov@gmail.com>
2025-03-24 09:34:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  return 47;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  return 47;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  return 47;
 }
