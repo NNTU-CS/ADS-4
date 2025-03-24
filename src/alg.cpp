00042526cc582f55edcb75d22af843bea0696600
Van4e7 <ivanzvectov@gmail.com>
2025-03-24 18:13:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,39 +1,41 @@
 // Copyright 2021 NNTU-CS
-#include <unordered_set>
 
-int countPairs1(int *arr, int len, int value) {
+int countPairs1(int const *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; ++i)
-  {
-      for (int j = i + 1; j < len; ++j)
-      {
-          if (arr[i] + arr[j] == value)
-          {
+  for (int i = 0; i < len; ++i) {
+      for (int j = i + 1; j < len; ++j) {
+          if (arr[i] + arr[j] == value) {
               count++;
           }
       }
   }
   return count;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int const *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
-  while (left < right)
-  {
+  while (left < right) {
       int sum = arr[left] + arr[right];
-      if (sum == value)
-      {
-          count++;
+      if (sum == value) {
+          int leftCount = 1;
+          int rightCount = 1;
+          while (left + 1 < right && arr[left] == arr[left + 1]) {
+              leftCount++;
+              left++;
+          }
+          while (right - 1 > left && arr[right] == arr[right - 1]) {
+              rightCount++;
+              right--;
+          }
+          count += leftCount * rightCount; 
           left++;
           right--;
       }
-      else if (sum < value)
-      {
+      else if (sum < value) {
           left++;
       }
-      else
-      {
+      else {
           right--;
       }
   }
@@ -41,15 +43,20 @@ int countPairs2(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-  std::unordered_set<int> seen;
-  for (int i = 0; i < len; ++i)
-  {
-      int complement = value - arr[i];
-      if (seen.count(complement))
-      {
-          count++;
+  int frequency[1001] = { 0 }; 
+  for (int i = 0; i < len; i++) {
+    frequency[arr[i]]++;
+  }
+  for (int i = 0; i <= value / 2; i++) {
+      int j = value - i;
+      if (j <= 1000) {
+          if (i == j) {
+              count += (frequency[i] * (frequency[i] - 1)) / 2;
+          }
+          else {
+              count += frequency[i] * frequency[j];
+          }
       }
-      seen.insert(arr[i]);
   }
   return count;
 }
