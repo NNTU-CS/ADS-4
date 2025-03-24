b3d09db2ad45a34fb81cd2e59807e04dd0dc6e0d
Van4e7 <ivanzvectov@gmail.com>
2025-03-24 19:52:33 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -1,11 +1,9 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
-#include <iostream>
-#include <unordered_map>
 
-int countPairs1(int *arr, int len, int value);
-int countPairs2(int *arr, int len, int value);
-int countPairs3(int *arr, int len, int value);
+int countPairs1(int const *arr, int len, int value);
+int countPairs2(int const *arr, int len, int value);
+int countPairs3(int const *arr, int len, int value);
 
 #endif  // INCLUDE_ALG_H_
