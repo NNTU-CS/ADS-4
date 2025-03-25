95cade210855f4040674bb9e8232e25da48abc77
pavuko <99765321+pavuko@users.noreply.github.com>
2025-03-25 22:31:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,63 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value)count++;
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int k = len - 1;
+  while (k > 0) {
+    if (arr[k] > value) {
+      k--;
+    } else {
+      break;
+    }
+  }
+  for (int i = 0; i < len; i++) {
+    for (int j = k; j > i; j--) {
+      if (arr[i] + arr[j] == value) count++;
+    }
+  }
+  return count;
 }
+int binary(int *arr, int low, int high, int value) {
+  int first = -1;
+  int left = low, right = high;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] >= value) {
+      right = mid - 1;
+      if (arr[mid] == value) first = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+  if (first == -1) return 0;
+  int last = first;
+  left = first;
+  right = high;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] <= value) {
+      left = mid + 1;
+      if (arr[mid] == value) last = mid;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return last - first + 1;
+}
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    count += binary(arr, i + 1, len - 1, value - arr[i]);
+  }
+  return count;
 }
