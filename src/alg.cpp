f5a682249aaa8b0e1922cf092e2e1ea440739671
Van4e7 <ivanzvectov@gmail.com>
2025-03-25 15:51:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,35 +11,13 @@ int countPairs1(int *arr, int len, int value) {
   }
   return count;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int* arr, int len, int value) {
   int count = 0;
-  int left = 0;
-  int right = len - 1;
-  while (left < right) {
-      int sum = arr[left] + arr[right];
-      if (sum < value) {
-          left++;
-      } else if (sum > value) {
-          right--;
-      } else {
-          if (arr[left] == arr[right]) {
-              int n = right - left + 1;
-              count += (n * (n - 1)) / 2;
-              break;
-          } else {
-              int countLeft = 1;
-              while (left + 1 < right && arr[left] == arr[left + 1]) {
-                  countLeft++;
-                  left++;
-              }
-              int countRight = 1;
-              while (right - 1 > left && arr[right] == arr[right - 1]) {
-                  countRight++;
-                  right--;
-              }
-              count += (countLeft * countRight);
-              left++;
-              right--;
+  for (int i = 0; i < len - 1; i++)
+  {
+      for (int j = len - 1; j > i; j--) {
+          if (arr[i] + arr[j] == value) {
+              count++;
           }
       }
   }
@@ -80,7 +58,7 @@ int binarySearchUpper(int* arr, int low, int high, int key) {
 
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len/16; i++) {
+  for (int i = 0; i < len-1; i++) {
       int target = value - arr[i];
       int lower = binarySearchLower(arr, i + 1, len - 1, target);
       if (lower != -1) {
