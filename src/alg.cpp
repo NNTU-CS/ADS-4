fe1bde72f1ce989b4ba1331fba9ca3c12137bcc1
BushSasha <sashabush2006@gmail.com>
2025-03-28 00:48:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,59 +1,58 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int count1 = 0;
+  int count = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value) {
-        ++count1;
+        ++count;
         break;
       }
     }
   }
-  return count1;
+  return count;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  int count2 = 0;
-  int leftin = 0;
-  int rightin = len - 1;
-  while (leftin < rightin) {
-    int summ = arr[leftin] + arr[rightin];
+  int count = 0;
+  int left_inde = 0;
+  int right_inde = len - 1;
+  while (left_inde < right_inde) {
+    int summ = arr[left_inde] + arr[right_inde];
     if (summ == value) {
-      ++count2;
-      ++leftin;
-      --rightin;
+      ++count;
+      ++left_inde;
+      --right_inde;
     } else if (summ < value) {
-      ++leftin;
+      ++left_inde;
     } else {
-      --rightin;
+      --right_inde;
     }
   }
-  return count2;
+  return count;
 }
 
-int binarySecondSearch(int *arr, int leftin, int rightin, int target) {
-  while (leftin <= rightin) {
-    int midin = leftin + (rightin - leftin) / 2;
-    if (arr[midin] == target) {
-      return midin;
-    } else if (arr[midin] < target) {
-      leftin = midin + 1;
+int binarySecondSearch(int *arr, int left_inde, int right_inde, int target) {
+  while (left_inde <= right_inde) {
+    int mid_inde = left_inde + (right_inde - left_inde) / 2;
+    if (arr[mid_inde] == target) {
+      return mid_inde;
+    } else if (arr[mid_inde] < target) {
+      left_inde = mid_inde + 1;
     } else {
-      rightin = midin - 1;
+      right_inde = mid_inde - 1;
     }
   }
   return -1;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int count3 = 0;
+  int count = 0;
   for (int i = 0; i < len; ++i) {
-        int complement = value - arr[i];
-        if (binarySecondSearch(arr, i + 1, len - 1, complement) != -1) {
-            ++count3;
-        }
+    int complement = value - arr[i];
+    if (binarySecondSearch(arr, i + 1, len - 1, complement) != -1) {
+      ++count;
     }
-    return count3;
+  }
+  return count;
 }
-
 
