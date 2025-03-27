48e01045f9c43c74fcdd631f704b8a605a136cc2
kre1m <aksenovnikita2006@gmail.com>
2025-03-28 01:52:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,5 @@
 // Copyright 2021 NNTU-CS
-#include <algorithm>
-
-int countPairs1(int* arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
@@ -12,8 +10,7 @@ int countPairs1(int* arr, int len, int value) {
   }
   return count;
 }
-int countPairs2(int* arr, int len, int value) {
-  std::sort(arr, arr + len);
+int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
@@ -23,18 +20,15 @@ int countPairs2(int* arr, int len, int value) {
       count++;
       left++;
       right--;
-    }
-    else if (sum < value) {
+    } else if (sum < value) {
       left++;
-    }
-    else {
+    } else {
       right--;
     }
   }
   return count;
 }
-int countPairs3(int* arr, int len, int value) {
-  std::sort(arr, arr + len);
+int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     int target = value - arr[i];
@@ -46,11 +40,9 @@ int countPairs3(int* arr, int len, int value) {
       if (arr[mid] == target) {
         count++;
         break;
-      }
-      else if (arr[mid] < target) {
-        left = mid + 1
-          }
-      else {
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
         right = mid - 1;
       }
     }
