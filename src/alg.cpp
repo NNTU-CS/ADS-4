d75b442eddfba0a42ce7a16e237e7cec5cc751c5
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-03-27 15:55:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,13 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
-    for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
+  int cnt = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value)cnt++;
         }
     }
-    return count;
+    return cnt;
 }
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
