8864f27c499a7b2e3da762fe4f5ad6b7375de067
kre1m <aksenovnikita2006@gmail.com>
2025-03-28 01:59:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,12 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1]) continue;
     for (int j = i + 1; j < len; ++j) {
+      if (j > i + 1 && arr[j] == arr[j - 1]) continue;
       if (arr[i] + arr[j] == value) {
         count++;
       }
@@ -10,16 +14,17 @@ int countPairs1(int *arr, int len, int value) {
   }
   return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int left = 0;
-  int right = len - 1;
+  int left = 0, right = len - 1;
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
       count++;
-      left++;
-      right--;
+      int prevLeft = arr[left], prevRight = arr[right];
+      while (left < right && arr[left] == prevLeft) left++;
+      while (left < right && arr[right] == prevRight) right--;
     } else if (sum < value) {
       left++;
     } else {
@@ -28,13 +33,13 @@ int countPairs2(int *arr, int len, int value) {
   }
   return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
+    if (i > 0 && arr[i] == arr[i - 1]) continue;
     int target = value - arr[i];
-    if (target < arr[i]) continue;
-    int left = i + 1;
-    int right = len - 1;
+    int left = i + 1, right = len - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == target) {
