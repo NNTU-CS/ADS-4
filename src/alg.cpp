ea9d1578708fdc01e76473e738fde210bb1c6ef5
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-03-27 16:33:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,12 @@
 
 int countPairs1(int *arr, int len, int value) {
   int cnt = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value)cnt++;
-        }
-    }
-    return cnt;
+  for (int i = 0; i < len; i++) {
+      for (int j = i + 1; j < len; j++) {
+          if (arr[i] + arr[j] == value)cnt++;
+      }
+  }
+  return cnt;
 }
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
@@ -42,7 +42,7 @@ int binarySearch(int *arr, int left, int right, int target) {
     }
     return -1;
 }
-int countPairs3(int *arr, int lower, int high, int value) {
+int countPairs3(int *arr, int lenf, int value) {
   int cnt = 0;
   for (int i = 0; i < lenf; i++) {
     cnt += binary(arr, i+1, lenf - 1, value - arr[i]);
