023ed08b2a3b8b62dfd50145117aa60d45135663
kre1m <aksenovnikita2006@gmail.com>
2025-03-28 02:45:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,8 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-
-int countPairs1(int* arr, int len, int value) {
+int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; ++i) {
-    if (i > 0 && arr[i] == arr[i - 1]) continue;
+  for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
-      if (j > 1 + 1 && arr[j] == arr[j - 1]) continue;
       if (arr[i] + arr[j] == value) {
         count++;
       }
@@ -14,17 +10,16 @@ int countPairs1(int* arr, int len, int value) {
   }
   return count;
 }
-
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  int left = 0, right = len - 1;
+  int left = 0;
+  int right = len - 1;
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
       count++;
-      int prevLeft = arr[left], prevRight = arr[right];
-      while (left < right && arr[left] == prevLeft) left++;
-      while (left < right && arr[right] == prevRight) right--;
+      left++;
+      right--;
     } else if (sum < value) {
       left++;
     } else {
@@ -33,26 +28,17 @@ int countPairs2(int *arr, int len, int value) {
   }
   return count;
 }
-
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    if (i > 0 && arr[i] == arr[i - 1]) continue;
     int target = value - arr[i];
-    int left = i + 1, right = len - 1;
+    if (target < arr[i]) continue;
+    int left = i + 1;
+    int right = len - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == target) {
         count++;
-        int l = mid - 1, r = mid + 1;
-        while (l >= left && arr[l] == target) {
-          count++;
-          l--;
-        }
-        while (r <= right && arr[r] == target) {
-          count++;
-          r++;
-        }
         break;
       } else if (arr[mid] < target) {
         left = mid + 1;
