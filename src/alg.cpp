7683c10547afe64824e89df209318d32e8cd3e94
Andrey20-07 <mr.klach999@gmail.com>
2025-03-29 11:39:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,5 @@
 // Copyright 2021 NNTU-CS
-void insertionSort(int* arr, int len) {
-  for (int i = 1; i < len; i++) {
-    int key = arr[i];
-    int j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = key;
-  }
-}
 int countPairs1(int *arr, int len, int value) {
-  insertionSort(arr, len);
   int count = 0;
   for (int i = 0; i < len; i++) {
     for (int j = i+1; j < len; j++) {
@@ -23,7 +11,6 @@ int countPairs1(int *arr, int len, int value) {
   return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  insertionSort(arr, len);
   int temp;
   int left = 0;
   int right = len-1;
@@ -33,7 +20,7 @@ int countPairs2(int *arr, int len, int value) {
     if (sum == value) {
       count++;
       temp = right-1;
-      while (arr[left] + arr[temp] == value) {
+      while ((left < temp) && (arr[left] + arr[temp] == value)) {
         count++;
         temp--;
       }
@@ -47,14 +34,13 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  insertionSort(arr, len);
   int count = 0;
   for (int i = 0; i < len - 1; i++) {
     int complement = value - arr[i];
     for (int j = i + 1; j < len; j++) {
       if (arr[j] == complement) {
         count++;
-        while (j + 1 < len && arr[j] == arr[j + 1]) {
+        while ((j + 1 < len) && (arr[j] == arr[j + 1])) {
           j++;
           count++;
         }
