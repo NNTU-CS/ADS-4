c210256f86e1164ac94974aef73c603f0dffa25b
KorobovaKate <kat3kor@yandex.ru>
2025-03-30 01:22:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -53,6 +53,7 @@ int countPairs2(int *arr, int len, int value) {
     
     return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
