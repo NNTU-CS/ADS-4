7d29bfda8ca37b7f922e2264b98f576ff7ebc2a5
BushSasha <sashabush2006@gmail.com>
2025-03-30 19:18:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,19 +14,24 @@ int countPairs1(int *arr, int len, int value) {
 
 int countPairs2(int *arr, int len, int value) {
   int count2 = 0;
-  int finall = len - 1;
-  while (finall > 0) {
-    if (arr[finall] > 0) {
-      finall--;
-    } else {
-      break;
-    }
-  }
-  for (int i = 0; i < len; i++) {
-    for (int j = finall; j > i; j--) {
-      if (arr[i] + arr[j] == value) {
-        count2++;
+  int left_inde = 0;
+  int right_inde = len - 1;
+  while (left_inde < right_inde) {
+    int summ = arr[left_inde] + arr[right_inde];
+    if (summ == value) {
+      ++count2;
+      int currentLeft_inde = arr[left_inde];
+      int currentRight_inde = arr[right_inde];
+      while (left_inde < right_inde && arr[left_inde] == currentLeft_inde) {
+        ++left_inde;
       }
+      while (left_inde < right_inde && arr[right_inde] == currentRight_inde) {
+        --right_inde;
+      }
+    } else if (summ < value) {
+      ++left_inde;
+    } else {
+      --right_inde;
     }
   }
   return count2;
@@ -48,8 +53,15 @@ int binarySecondSearch(int *arr, int left_inde, int right_inde, int target) {
 
 int countPairs3(int *arr, int len, int value) {
   int count3 = 0;
-  for (int i = 0; i < len; i++) {
-    count3 += binarySecondSearch(arr, i + 1, len - 1, value - arr[i]);
+  for (int i = 0; i < len; ++i) {
+    int complement = value - arr[i];
+    int pos = binarySearch(arr, i + 1, len - 1, complement);
+    if (pos != -1) {
+      ++count;
+      while (i + 1 < len && arr[i] == arr[i + 1]) {
+        ++i;
+      }
+    }
   }
   return count3;
 }
