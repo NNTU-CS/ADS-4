39e42156fe2d04b3656ea7ec11fd0af403ceae22
Eternity8c <morozkind6@gmail.com>
2025-03-30 19:29:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,19 +11,16 @@ int countPairs1(int *arr, int len, int value) {
     return col;
 }
 int countPairs2(int *arr, int len, int value) {
-    int count = 0;
+    int col = 0;
     for (int i = 0; i < len - 1; i++) {
         int target = value - arr[i];
-        int j = len - 1;
-        while (j > i && arr[j] > target) {
-            j--;
-        }
-        while (j > i && arr[j] == target) {
-            count++;
-            j--;
+        for (int j = i + 1; j < len; j++) {
+            if (arr[j] == target) {
+                col++;
+            }
         }
     }
-    return count;
+    return col;
 }
 int countPairs3(int *arr, int len, int value) {
     int col = 0;
