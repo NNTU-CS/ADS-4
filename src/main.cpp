dfe9b50b52017b4491e286ae43cabe905a81d358
malishev-danil <malishev-danil5@yandex.ru>
2025-03-30 16:45:32 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int main() {
-    return 0;
+int arr[] = { 20, 30, 30, 40, 40 };
+int len = sizeof(arr) / sizeof(arr[0]);
+int value = 50;
+countPairs1(arr, len, value);
+countPairs2(arr, len, value);
+countPairs3(arr, len, value);
+return 0;
 }
