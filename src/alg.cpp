706c2123ab0260e84b757b3fbd4ba2d0e38475e8
malishev-danil <malishev-danil5@yandex.ru>
2025-03-30 15:35:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value) {
+int countPairs1(const int* arr, int len, int value) {
 int count1 = 0;
 for (int i = 0; i < len; ++i) {
 for (int j = i + 1; j < len; ++j) {
@@ -10,7 +10,7 @@ count1++;
 }
 return count1;
 }
-int countPairs2(int* arr, int len, int value) {
+int countPairs2(const int* arr, int len, int value) {
 int count2 = 0;
 int left = 0, right = len - 1;
 while (left < right) {
@@ -37,7 +37,7 @@ right--;
 }
 return count2;
 }
-int binarySearch(int* arr, int left, int right, int value) {
+int binarySearch(const int* arr, int left, int right, int value) {
 while (left <= right) {
 int mid = left + (right - left) / 2;
 if (arr[mid] == value) {
@@ -50,13 +50,13 @@ right = mid - 1;
 }
 return -1;
 }
-int countPairs3(int* arr, int len, int value) {
+int countPairs3(const int* arr, int len, int value) {
 int count3 = 0;
 for (int i = 0; i < len; ++i) {
 int target = value - arr[i];
 int idx = binarySearch(arr, i + 1, len - 1, target);
 if (idx != -1) {
-count3++;		
+count3++;
 int left = idx - 1;
 while (left > i && arr[left] == target) {
 count3++;
