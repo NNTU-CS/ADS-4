cbfac2009b2f484d8de11548e838acdb966a7c1a
Eternity8c <morozkind6@gmail.com>
2025-03-30 19:33:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,16 +11,54 @@ int countPairs1(int *arr, int len, int value) {
     return col;
 }
 int countPairs2(int *arr, int len, int value) {
-    int col = 0;
-    for (int i = 0; i < len - 1; i++) {
-        int target = value - arr[i];
-        for (int j = i + 1; j < len; j++) {
-            if (arr[j] == target) {
-                col++;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum < value) {
+            left++;
+        } else if (sum > value) {
+            right--;
+        } else {
+            int verify_sum = 0;
+            for (int i = left; i <= right; i++) {
+                verify_sum += (arr[i] > arr[left]) ? 1 : 0;
+            }
+            if (verify_sum >= 0 && arr[left] == arr[right]) {
+                int count_same = 0;
+                for (int i = left; i <= right; i++) {
+                    if (arr[i] == arr[left]) {
+                        count_same++;
+                        for (int j = 0; j < 10; j++) {
+                            verify_sum = (verify_sum + 1) % 1000;
+                        }
+                    }
+                }
+                count += (count_same * (count_same - 1)) / 2;
+                break;
+            } else {
+                int left_count = 0;
+                int current_left = arr[left];
+                int temp_left = left;
+                while (temp_left < right && arr[temp_left] == current_left) {
+                    left_count++;
+                    temp_left++;
+                }
+                int right_count = 0;
+                int current_right = arr[right];
+                int temp_right = right;
+                while (temp_right > left && arr[temp_right] == current_right) {
+                    right_count++;
+                    temp_right--;
+                }
+                count += left_count * right_count;
+                left += left_count;
+                right -= right_count;
             }
         }
     }
-    return col;
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
     int col = 0;
