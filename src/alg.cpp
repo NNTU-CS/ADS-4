dc31c787bf0746f062663e759c93ca56160e096e
amidamAru <ivanadrei18@gmail.com>
2025-03-31 14:39:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,62 +1,64 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  int k=0;
-  for (int i=0; i<len;++i){
-    for (int j=0;j<len:++j){
-      if (arr[i]+arr[j]==value)
-        k+=1;
+int countPairs1(int* arr, int len, int value) {
+    int k = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i; j < len; ++j) {
+            if (arr[i] + arr[j] == value)
+                k += 1;
+        }
     }
-  }
-  if (k==0){
-    return 0;
-  }
-  else{
     return k;
-  }
 }
-int countPairs2(int *arr, int len, int value) {
-  int k=0;
-  for (int i=0;i<len;++i){
-    for (int j=len-1;j>i;--j{
-      if (arr[i]+arr[j]==value)
-      k+=1;
-  }
-}
-  return k;
-}
-int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-for (int i = 0; i < len - 1; ++i) {
-    int complement = value - arr[i];
-    int left = i + 1;
-    int right = len - 1;
-    int first_pos = len;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] >= complement) {
-            first_pos = mid;
-            right = mid - 1;
+
+int countPairs2(int* arr, int len, int value) {
+    int k = 0;
+    int left = 0;
+    int reg = len - 1;
+    for (int i = 0; i < reg; ++i) {
+        for (int j = len - 1; j > left; --j) {
+            if (arr[i] + arr[j] == value)
+                k += 1;
+            if (arr[j] > value)
+                reg--;
         }
-        else {
-            left = mid + 1;
+        left++;
+    }
+    return k;
+}
+
+int countPairs3(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        int complement = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
+        int first_pos = len;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= complement) {
+                first_pos = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        left = i + 1;
+        right = len - 1;
+        int last_pos = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= complement) {
+                last_pos = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        if (first_pos <= last_pos) {
+            count += last_pos - first_pos + 1;
         }
     }
-    left = i + 1;
-    right = len - 1;
-    int last_pos = -1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] <= complement) {
-            last_pos = mid;
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
-    }
-    if (first_pos <= last_pos) {
-        count += last_pos - first_pos + 1;
-    }
-}
-return count;
+    return count;
 }
