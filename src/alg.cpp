73c8d6eed63aa4a0f170f60a87ee01a982e63345
letmebleedout <199362693+letmebleedout@users.noreply.github.com>
2025-03-31 16:46:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,43 +15,40 @@ int count = 0, left = 0, right = len - 1;
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      if (arr[left]==arr[right]) {
-        int num = right - left + 1;
-        count += num * (num - 1) / 2;
+      int cntLeft = 1, cntRight = 1;
+      while (left + cntLeft < right && arr[left + cntLeft] == arr[left]) cntLeft++;
+      while (right - cntRight > left && arr[right - cntRight] == arr[right]) cntRight++;
+      if (arr[left] == arr[right]) {
+        count += (right - left + 1) * (right - left) / 2;
         break;
       } else {
-        int countLeft = 1, countRight = 1;
-        while (left + 1 < right && arr[left] == arr[left + 1]) {
-          countLeft++;
-          left++;
-        }
-        while (right - 1 > left && arr[right] == arr[right - 1]) {
-          countRight++;
-          right--;
-        }
-        count += countLeft * countRight;
-        left++;
-        right--;
+        count += cntLeft * cntRight;
+        left += cntLeft;
+        right -= cntRight;
       }
-    } else if (sum < value)
+    } else if (sum < value) {
       left++;
-    else
+    } else {
       right--;
+    }
   }
   return count;
 }
 int countPairs3(int *arr, int len, int value) {
-    int count = 0;
+  int count = 0;
   for (int i = 0; i < len - 1; i++) {
     int target = value - arr[i];
     int left = i + 1, right = len - 1;
     while (left <= right) {
-      int mid = left + ((right - left) >> 1);
+      int mid = left + ((right - left) / 2);
       if (arr[mid] == target) {
         count++;
         break;
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
-      arr[mid] < target ? left = mid + 1 : right = mid - 1;
     }
   }
   return count;
