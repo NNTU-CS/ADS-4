2f0ac110e9c3f0ed2e437f3dcba7885ff4c099fe
letmebleedout <199362693+letmebleedout@users.noreply.github.com>
2025-03-31 17:25:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,19 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < len - 1; i++) {
     for (int j = i + 1; j < len; j++) {
       if (arr[i] + arr[j] == value) {
         count++;
-        while (j + 1 < len && arr[j] == arr[j + 1]) j++;
       }
     }
-    while (i + 1 < len && arr[i] == arr[i + 1]) i++;
   }
   return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0, left = 0, right = len - 1;
+  int count = 0;
+  int left = 0, right = len - 1;
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
@@ -23,10 +23,8 @@ int countPairs2(int *arr, int len, int value) {
         break;
       } else {
         int cntLeft = 1, cntRight = 1;
-        while (left + cntLeft < right && arr[left + cntLeft] == arr[left]) 
-          cntLeft++;
-        while (right - cntRight > left && arr[right - cntRight] == arr[right]) 
-          cntRight++;
+        while (left + cntLeft < right && arr[left + cntLeft] == arr[left]) cntLeft++;
+        while (right - cntRight > left && arr[right - cntRight] == arr[right]) cntRight++;
         count += cntLeft * cntRight;
         left += cntLeft;
         right -= cntRight;
@@ -39,17 +37,18 @@ int countPairs2(int *arr, int len, int value) {
   }
   return count;
 }
-int countOccurrences(int *arr, int low, int high, int value) {
-  int countOccurrences(int *arr, int left, int right, int target) {
+int countOccurrences(int *arr, int left, int right, int target) {
   int low = left, high = right, mid;
   int first = -1, last = -1;
   while (low <= high) {
     mid = low + (high - low) / 2;
-    if (arr[mid] >= target) {
-      if (arr[mid] == target) first = mid;
+    if (arr[mid] == target) {
+      first = mid;
       high = mid - 1;
-    } else {
+    } else if (arr[mid] < target) {
       low = mid + 1;
+    } else {
+      high = mid - 1;
     }
   }
   if (first == -1)
@@ -57,8 +56,10 @@ int countOccurrences(int *arr, int low, int high, int value) {
   low = first, high = right;
   while (low <= high) {
     mid = low + (high - low) / 2;
-    if (arr[mid] <= target) {
-      if (arr[mid] == target) last = mid;
+    if (arr[mid] == target) {
+      last = mid;
+      low = mid + 1;
+    } else if (arr[mid] < target) {
       low = mid + 1;
     } else {
       high = mid - 1;
