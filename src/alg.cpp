0228ae86306f766cc6d7b53c3adb303300153f6a
yeefip <yeefipp@gmail.com>
2025-03-31 09:57:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,11 +5,11 @@
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue; // Пропуск дубликатов
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
         for (int j = i + 1; j < len; j++) {
+            if (j > i + 1 && arr[j] == arr[j - 1]) continue;
             if (arr[i] + arr[j] == value) {
                 count++;
-                break; // Уникальные пары, пропускаем повторения
             }
         }
     }
@@ -22,9 +22,8 @@ int countPairs2(int *arr, int len, int value) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
             count++;
-            int lastLeft = arr[left], lastRight = arr[right];
-            while (left < right && arr[left] == lastLeft) left++;
-            while (left < right && arr[right] == lastRight) right--;
+            do { left++; } while (left < right && arr[left] == arr[left - 1]);
+            do { right--; } while (left < right && arr[right] == arr[right + 1]);
         } else if (sum < value) {
             left++;
         } else {
@@ -47,7 +46,7 @@ int binarySearch(int *arr, int left, int right, int target) {
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue; // Пропуск дубликатов
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
         int complement = value - arr[i];
         if (binarySearch(arr, i + 1, len - 1, complement) != -1) {
             count++;
