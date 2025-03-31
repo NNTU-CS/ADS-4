131f75e786cd6f8b96c048d355c0b750037a8565
letmebleedout <199362693+letmebleedout@users.noreply.github.com>
2025-03-31 17:02:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,23 +1,29 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
+  for (int i = 0; i < len - 1; i++)
+    for (int j = i + 1; j < len; j++)
+      if (arr[i] + arr[j] == value)
         count++;
-      }
-    }
-  }
   return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  int count = 0, left = 0, right = len - 1;
+  int count = 0;
+  int left = 0, right = len - 1;
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
-      count++;
-      left++;
-      right--;
+      if (arr[left] == arr[right]) {
+        int num = right - left + 1;
+        count += num * (num - 1) / 2;
+        break;
+      }
+      int cntLeft = 1, cntRight = 1;
+      while (left + cntLeft < right && arr[left + cntLeft] == arr[left]) cntLeft++;
+      while (right - cntRight > left && arr[right - cntRight] == arr[right]) cntRight++;
+      count += cntLeft * cntRight;
+      left += cntLeft;
+      right -= cntRight;
     } else if (sum < value) {
       left++;
     } else {
@@ -31,13 +37,29 @@ int countPairs3(int *arr, int len, int value) {
   for (int i = 0; i < len - 1; i++) {
     int target = value - arr[i];
     int left = i + 1, right = len - 1;
+    bool found = false;
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (arr[mid] == target) {
-        count++;
+        found = true;
+        int cnt = 1;
+        int l = mid - 1;
+        while (l > i && arr[l] == target) {
+          cnt++;
+          l--;
+        }
+        int r = mid + 1;
+        while (r < len && arr[r] == target) {
+          cnt++;
+          r++;
+        }
+        count += cnt;
         break;
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
-      (arr[mid] < target) ? left = mid + 1 : right = mid - 1;
     }
   }
   return count;
