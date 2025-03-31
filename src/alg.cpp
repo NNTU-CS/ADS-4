35e9824cef06d4680e1264fc62d1e6a12e0703a9
yeefip <yeefipp@gmail.com>
2025-03-31 10:02:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,14 +4,14 @@
 
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
+    for (int i = 0; i < len - 1; i++) {
         for (int j = i + 1; j < len; j++) {
-            if (j > i + 1 && arr[j] == arr[j - 1]) continue;
             if (arr[i] + arr[j] == value) {
                 count++;
+                while (j + 1 < len && arr[j] == arr[j + 1]) j++; // Пропуск дубликатов
             }
         }
+        while (i + 1 < len && arr[i] == arr[i + 1]) i++; // Пропуск дубликатов
     }
     return count;
 }
@@ -22,12 +22,9 @@ int countPairs2(int *arr, int len, int value) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
             count++;
-            do {
-                left++;
-            } while (left < right && arr[left] == arr[left - 1]);
-            do {
-                right--;
-            } while (left < right && arr[right] == arr[right + 1]);
+            while (left < right && arr[left] == arr[left + 1]) left++; // Пропуск дубликатов
+            while (left < right && arr[right] == arr[right - 1]) right--; // Пропуск дубликатов
+            left++, right--;
         } else if (sum < value) {
             left++;
         } else {
@@ -53,16 +50,12 @@ int binarySearch(int *arr, int left, int right, int target) {
 
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
+    for (int i = 0; i < len - 1; i++) {
         int complement = value - arr[i];
-        int foundIndex = binarySearch(arr, i + 1, len - 1, complement);
-        if (foundIndex != -1) {
+        if (binarySearch(arr, i + 1, len - 1, complement) != -1) {
             count++;
-            while (foundIndex + 1 < len && arr[foundIndex] == arr[foundIndex + 1]) {
-                foundIndex++;
-            }
         }
+        while (i + 1 < len && arr[i] == arr[i + 1]) i++; // Пропуск дубликатов
     }
     return count;
 }
