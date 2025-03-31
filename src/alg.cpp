f8b4594d1a8561c2047adb57f5efb7ad8bc7a879
Nikolay <99762459+Tidud@users.noreply.github.com>
2025-03-31 19:48:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    for (int j = 0; j < len; ++j) {
+    for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value) ++count;
     }
   }
@@ -21,22 +21,18 @@ int countPairs2(int* arr, int len, int value) {
 int countPairs3(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    int lbound = i, rbound = len - 1, target = value - arr[i];
-    while (lbound < rbound) {
-      if ((rbound - lbound) / 2 + lbound == target) {
+    int lbound = i + 1, rbound = len - 1, target = value - arr[i];
+    while (lbound <= rbound) {
+      if (arr[(rbound - lbound) / 2 + lbound] == target) {
         ++count;
         int current = (rbound - lbound) / 2 + lbound;
-        while (true) {
-          if (++current < rbound && arr[current] == target) ++count;
-          else break;
-        }
+        while (++current < rbound && arr[current] == target) ++count;
         current = (rbound - lbound) / 2 + lbound;
-        while (true) {
-          if (--current > lbound && arr[current] == target) ++count;
-          else break;
-        }
+        while (--current > lbound && arr[current] == target) ++count;
       }
-      else if ((rbound - lbound) / 2 + lbound > target) rbound = (rbound - lbound) / 2 + lbound - 1;
+      else if ((rbound - lbound) / 2 + lbound > target) {
+        rbound = (rbound - lbound) / 2 + lbound - 1;
+      }
       else lbound = (rbound - lbound) / 2 + lbound + 1;
     }
   }
