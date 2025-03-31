9b32bb0166c67679c2bd47e0c1a9a0cdb810a358
amidamAru <ivanadrei18@gmail.com>
2025-03-31 17:06:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,5 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
+int countPairs1(int* arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
@@ -12,28 +12,28 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int* arr, int len, int value) {
-  int count = 0;
-  int lboard = 0;
-  int rboard = len - 1;
-  for (int i = 0; i < rboard; ++i)
-  {
-    for (int j = rboard-1; j>lboard; --j)
+    int count = 0;
+    int lboard = 0;
+    int rboard = len - 1;
+    for (int i = 0; i < rboard; ++i)
     {
-      if (arr[j]>value)
-      {
-        rboard -= 1;
-      }
-      if (arr[i] + arr[j] == value)
-      {
-        count += 1;
-      }
+        for (int j = rboard - 1; j > lboard; --j)
+        {
+            if (arr[j] > value)
+            {
+                rboard -= 1;
+            }
+            if (arr[i] + arr[j] == value)
+            {
+                count += 1;
+            }
+        }
+        lboard += 1;
     }
-    lboard += 1;
-  }
-  return count;
+    return count;
 }
 
-int binarySearch(int *arr, int left, int right, int target, bool find_first) {
+int binarySearch(int* arr, int left, int right, int target, bool find_first) {
     int result = -1;
     while (left <= right) {
         int mid = left + (right - left) / 2;
@@ -41,19 +41,22 @@ int binarySearch(int *arr, int left, int right, int target, bool find_first) {
             result = mid;
             if (find_first) {
                 right = mid - 1;
-            } else {
+            }
+            else {
                 left = mid + 1;
             }
-        } else if (arr[mid] < target) {
+        }
+        else if (arr[mid] < target) {
             left = mid + 1;
-        } else {
+        }
+        else {
             right = mid - 1;
         }
     }
     return result;
 }
 
-int countPairs3(int *arr, int len, int value) {
+int countPairs3(int* arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
         int complement = value - arr[i];
