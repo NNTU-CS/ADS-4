98aa2e35c057d85a81eb4157d5734eff48506c3b
Nikolay <99762459+Tidud@users.noreply.github.com>
2025-03-31 20:24:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -31,23 +31,10 @@ int countPairs3(int* arr, int len, int value) {
         current = mid;
         while (--current >= lbound && arr[current] == target) ++count;
         break;
-      } 
-      if (arr[mid] > target) rbound = mid - 1; 
+      }
+      if (arr[mid] > target) rbound = mid - 1;
       if (arr[mid] < target) lbound = mid + 1;
     }
   }
   return count;
-}
-
-void generateSorted(int* arr, int min, int max, int len) {
-  int value = 0, j = 0;
-  for (int i = 0; i < len; i++) {
-    value = min + rand() % (max - min + 1);
-    j = i - 1;
-    while (j >= 0 && arr[j] > value) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = value;
-  }
 }
