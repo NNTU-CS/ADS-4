cc446238397ae38ff2ea48074d8f66b3616034e7
Анна <sol0dova.anna@yandex.ru>
2025-04-02 23:22:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -37,22 +37,22 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    int find = value - arr[i];
+    int complement = value - arr[i];
     int left = i + 1;
     int right = len - 1;
     while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] == find) {
-        while (arr[mid] == find) {
-          count++;
-          mid--;
-        }
-        break;
-      } else if (arr[mid] < find) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == complement) {
+      while (arr[mid] == complement) {
+        count++;
+        mid--;
       }
+      break;
+    } else if (arr[mid] < complement) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
     }
   }
   return count;
