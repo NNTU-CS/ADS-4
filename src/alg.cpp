18051ad8a83980c46d9705498392eb7316209755
KorobovaKate <kat3kor@yandex.ru>
2025-04-03 23:58:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -52,36 +52,38 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 // cppcheck-suppress constParameterPointer
-int countPairs3(int *arr, int len, int value) {
-    int kol = 0;
-    std::sort(arr, arr + len);
-    for (int i = 0; i < len; ++i) {
-        int target = value - arr[i];
-        int leftIndex = i + 1;
-        int right = len - 1;
-        int first = -1;
-        while (leftIndex <= right) {
-            int mid = leftIndex + (right - leftIndex) / 2;
-            if (arr[mid] >= target) {
-                right = mid - 1;
-                if (arr[mid] == target) first = mid;
-            } else {
-                leftIndex = mid + 1;
-            }
-        }
-        if (first == -1) continue;
-        int last = first;
-        leftIndex = first;
-        while (leftIndex <= right) {
-            int mid = leftIndex + (right - leftIndex) / 2;
-            if (arr[mid] <= target) {
-                leftIndex = mid + 1;
-                if (arr[mid] == target) last = mid;
-            } else {
-                right = mid - 1;
-            }
-        }
-        kol += (last - first + 1);
+int Binar_poisk(int *arr, int low, int high, int value) {
+  int first = -1;
+  int left = wit, right = high;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] >= value) {
+      right = mid - 1;
+      if (arr[mid] == value) first = mid;
+    } else {
+    left = mid + 1;
     }
-    return kol;
+  }
+  if (first == -1) return 0;
+  int last = first;
+  left = first;
+  right = high;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] <= value) {
+      left = mid + 1;
+      if (arr[mid] == value) last = mid;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return last - first + 1;
+}
+
+int countPairs3(int *arr, int len, int value) {
+  int kol = 0;
+  for (int i = 0; i < len; ++i) {
+    kol += Binar_poisk(arr, i + 1, len - 1, value - arr[i]);
+  }
+  return kol;
 }
