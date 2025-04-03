7d33368780b457191501c204b1f3e9456b237a07
Anitone <133121791+Anitone@users.noreply.github.com>
2025-04-03 15:33:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -44,16 +44,16 @@ int countPairs2(int* arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
-  int right = length - 1;
+  int right = len - 1;
   while (left < right) {
     int summ = arr[left] + arr[right];
-    if (summ == target) {
+    if (summ == value) {
       count++;
       while (left < right && arr[left] == arr[left + 1]) left++;
       while (left < right && arr[right] == arr[right - 1]) right--;
       left++;
       right--;
-    } else if (summ < target) {
+    } else if (summ < value) {
       left++;
     } else {
       right--;
