491fd8b3d4ffa49816f8d22e9cd23cced9bd29aa
Arseny <grigoriyevvv2@mail.ru>
2025-04-03 19:43:18 +0300

    You know who it

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,14 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for(int i = 0; i < len - 1; i++) {
+    for(int j = i; j < len; j++) {
+      if(arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
   return 0;
