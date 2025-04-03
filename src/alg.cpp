80749826f17db7edc86d6a5263060233f9844e62
Anitone <133121791+Anitone@users.noreply.github.com>
2025-04-03 14:48:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,37 +23,11 @@ int countPairs2(int *arr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-  int middle = len / 2;
-  if (middle * 2 != len){
-    int lMiddle = middle;
-    int rMiddle = middle + 1;
-    for (int i = 0; i < lMiddle; i++) {
-      for (int j = i + 1; j < lMiddle; j++) {
-        if (arr[i] + arr[j] == value) {
-          count++;
-        }
-      }
-    }
-    for (int i = len - 1; i >= rMiddle; i--) {
-      for (int j = i - 1; j >= rMiddle; j--) {
-        if (arr[i] + arr[j] == value) {
-          count++;
-        }
-      }
-    }
-  } else {
-    for (int i = 0; i < middle; i++) {
-      for (int j = i + 1; j < middle; j++) {
-        if (arr[i] + arr[j] == value) {
-          count++;
-        }
-      }
-    }
-    for (int i = len - 1; i >= middle; i--) {
-      for (int j = i - 1; j >= middle; j--) {
-        if (arr[i] + arr[j] == value) {
-          count++;
-        }
+  for (int i = 0; i < len; i++) {
+    int target = value - arr[i];
+    for (int j = i + 1; j < len; j++) {
+      if (arr[j] == target) {
+        count++;
       }
     }
   }
