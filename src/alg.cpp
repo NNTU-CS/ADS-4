a821e36eda924e44e57769849b47075887e4a3a3
danyabatow <danya.batov06@mail.ru>
2025-04-03 21:21:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,7 @@ int countPairs1(int *arr, int len, int value) {
   return count;
 }
 
-int countPairs2(int *arr, int len, int value) {
+int countPairs3(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
@@ -43,23 +43,27 @@ int countPairs2(int *arr, int len, int value) {
   return count;
 }
 
-int countPairs3(int *arr, int len, int value) {
+int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; ++i) {
-    int complement = value - arr[i];
+  for (int i = 0; i < len; i++) {
     int left = i + 1;
     int right = len - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (arr[mid] == complement) {
-        int countComplement = 1;
-        while (mid + 1 <= right && arr[mid + 1] == complement) {
-          countComplement++;
-          mid++;
+      if (arr[i] + arr[mid] == value) {
+        count++;
+        int l = mid - 1;
+        while (l >= left && arr[i] + arr[l] == value) {
+          count++;
+          l--;
         }
-        count += countComplement;
+        int r = mid + 1;
+        while (r <= right && arr[i] + arr[r] == value) {
+          count++;
+          r++;
+        }
         break;
-      } else if (arr[mid] < complement) {
+      } else if (arr[i] + arr[mid] < value) {
         left = mid + 1;
       } else {
         right = mid - 1;
