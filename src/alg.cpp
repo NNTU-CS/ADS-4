63e093f806ce10d74d435620c76e4c9e2e5dcda5
Анна <sol0dova.anna@yandex.ru>
2025-04-04 00:50:53 +0300

    ads-4

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,8 +22,7 @@ int countPairs2(int *arr, int len, int value) {
         int totalElements = right - left + 1;
         count += (totalElements * (totalElements - 1)) / 2;
         break;
-      }
-      else {
+      } else {
         int countLeft = 1;
         int countRight = 1;
         while (left + 1 <= right && arr[left] == arr[left + 1]) {
@@ -51,7 +50,7 @@ int countPairs3(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; i++) {
     int target = value - arr[i];
-    int left = 0;
+    int left = i;
     int right = len - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
@@ -60,16 +59,13 @@ int countPairs3(int* arr, int len, int value) {
         while (j < len && arr[j] == target) {
           j++;
         }
-        count += j - mid;
+        count += j - mid+1;
         break;
       } else if (arr[mid] < target) {
         left = mid + 1;
       } else {
         right = mid - 1;
       }
-    }
-    while (i + 1 < len && arr[i] == arr[i + 1]) {
-      i++;
     }
   }
   return count;
