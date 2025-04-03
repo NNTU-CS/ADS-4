4e670351fdbe683a1ea2d3fe88ee155be5b420c6
Arseny <grigoriyevvv2@mail.ru>
2025-04-03 19:46:38 +0300

    test

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for(int i = 0; i < len - 1; i++) {
-    for(int j = i; j < len; j++) {
-      if(arr[i] + arr[j] == value) {
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
         count++;
       }
     }
@@ -11,8 +11,24 @@ int countPairs1(int *arr, int len, int value) {
   return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
