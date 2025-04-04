8b47ff6ac9266a61b656c0ea99da6341c51b57e1
Arseny <grigoriyevvv2@mail.ru>
2025-04-04 17:19:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,23 +12,53 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
+  int end = len - 1;
+  while (end > 0) {
+    if (arr[end] > value) {
+      end--;
+    } else {
+      break;
+    }
+  }
   for (int i = 0; i < len; i++) {
-    for (int j = len - 1; arr[j] > arr[i]; j--) {
-      if (arr[i] + arr[j] == value) {
+    for (int j = end; j > i; j--) {
+      if (arr[i] + arr[j] == value)
         count++;
-      }
     }
   }
   return count;
 }
+int bsStep(int *arr, int left, int right, int value) {
+  int first = -1;
+  int _left = left, _right = right;
+  while (_left <= _right) {
+    int mid = _left + (_right - _left) / 2;
+    if (arr[mid] >= value) {
+      _right = mid - 1;
+      if (arr[mid] == value) first = mid;
+    } else {
+    _left = mid + 1;
+    }
+  }
+  if (first == -1) return 0;
+  int last = first;
+  _left = first;
+  _right = right;
+  while (_left <= _right) {
+    int mid = _left + (_right - _left) / 2;
+    if (arr[mid] <= value) {
+      _left = mid + 1;
+      if (arr[mid] == value) last = mid;
+    } else {
+      _right = mid - 1;
+    }
+  }
+  return last - first + 1;
+}
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count++;
-      }
-    }
+  for (int i = 0; i < len; ++i) {
+    count += binar_poisk(arr, i + 1, len - 1, value - arr[i]);
   }
   return count;
 }
