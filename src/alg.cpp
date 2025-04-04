ebaf7c6081de00ba13da3b3fb56dbf48dda2a836
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-04-04 20:25:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,9 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value) {
+#include <algorithm>
+int countPairs1(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
             if (arr[i] + arr[j] == value) {
                 count++;
             }
@@ -10,42 +11,76 @@ int countPairs1(int* arr, int len, int value) {
     }
     return count;
 }
-int countPairs2(int* arr, int len, int value) {
+int countPairs2(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = len - 1; j > i; j--) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            } else if (arr[i] + arr[j] < value) {
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                count += n * (n - 1) / 2;
                 break;
+            } else {
+                int left_val = arr[left];
+                int right_val = arr[right];
+                int left_count = 0;
+                int right_count = 0;
+                while (left < len && arr[left] == left_val) {
+                    left++;
+                    left_count++;
+                }
+                while (right >= 0 && arr[right] == right_val) {
+                    right--;
+                    right_count++;
+                }
+                count += left_count * right_count;
             }
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
         }
     }
+    
     return count;
 }
-int countPairs3(int* arr, int len, int value) {
+int countPairs3(int *arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; ++i) {
         int target = value - arr[i];
+        if (target < 0) continue;
         int left = i + 1;
         int right = len - 1;
-        
+        int first = -1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
-                count++;
-                for (int j = mid - 1; j >= left && arr[j] == target; j--) {
-                    count++;
-                }
-                for (int j = mid + 1; j <= right && arr[j] == target; j++) {
-                    count++;
-                }
-                break;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
+            if (arr[mid] >= target) {
                 right = mid - 1;
+                if (arr[mid] == target) {
+                    first = mid;
+                }
+            } else {
+                left = mid + 1;
             }
+        }
+        if (first != -1) {
+            left = first;
+            right = len - 1;
+            int last = first;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] <= target) {
+                    left = mid + 1;
+                    if (arr[mid] == target) {
+                        last = mid;
+                    }
+                } else {
+                    right = mid - 1;
+                }
+            }
+            count += last - first + 1;
         }
     }
     return count;
