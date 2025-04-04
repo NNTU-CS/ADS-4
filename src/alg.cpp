2326b97ff4b55bb2ddd2465f6a4a415028cac769
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-04-04 20:15:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,5 @@
 // Copyright 2021 NNTU-CS
-void quickSort(int* arr, int left, int right) {
-    while (left < right) {
-        int pivot = arr[(left + right) / 2];
-        int i = left;
-        int j = right;
-        while (i <= j) {
-            while (arr[i] < pivot) i++;
-            while (arr[j] > pivot) j--;
-            if (i <= j) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++;
-                j--;
-            }
-        }
-        if (j - left < right - i) {
-            quickSort(arr, left, j);
-            left = i;
-        } else {
-            quickSort(arr, i, right);
-            right = j;
-        }
-    }
-}
 int countPairs1(int* arr, int len, int value) {
-    quickSort(arr, 0, len - 1);
     int count = 0;
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
@@ -37,66 +11,40 @@ int countPairs1(int* arr, int len, int value) {
     return count;
 }
 int countPairs2(int* arr, int len, int value) {
-    quickSort(arr, 0, len - 1);
     int count = 0;
-    int left = 0;
-    int right = len - 1;
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            if (arr[left] == arr[right]) {
-                count += (right - left + 1) * (right - left) / 2;
+    for (int i = 0; i < len; i++) {
+        for (int j = len - 1; j > i; j--) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            } else if (arr[i] + arr[j] < value) {
                 break;
             }
-            int left_val = arr[left];
-            int right_val = arr[right];
-            int left_count = 0;
-            int right_count = 0;
-            
-            while (left < len && arr[left] == left_val) {
-                left_count++;
-                left++;
-            }
-            while (right >= 0 && arr[right] == right_val) {
-                right_count++;
-                right--;
-            }
-            count += left_count * right_count;
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
         }
     }
     return count;
 }
 int countPairs3(int* arr, int len, int value) {
-    quickSort(arr, 0, len - 1);
     int count = 0;
     for (int i = 0; i < len; i++) {
         int target = value - arr[i];
-        int low = i + 1;
-        int high = len - 1;
+        int left = i + 1;
+        int right = len - 1;
         
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (arr[mid] == target) {
                 count++;
-                int left = mid - 1;
-                while (left >= low && arr[left] == target) {
+                for (int j = mid - 1; j >= left && arr[j] == target; j--) {
                     count++;
-                    left--;
                 }
-                int right = mid + 1;
-                while (right <= high && arr[right] == target) {
+                for (int j = mid + 1; j <= right && arr[j] == target; j++) {
                     count++;
-                    right++;
                 }
                 break;
             } else if (arr[mid] < target) {
-                low = mid + 1;
+                left = mid + 1;
             } else {
-                high = mid - 1;
+                right = mid - 1;
             }
         }
     }
