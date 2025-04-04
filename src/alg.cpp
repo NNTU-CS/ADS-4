2bbbc89ca06c9047fa47ed7456ab92bd8c2dafcb
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-04-04 13:33:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,13 @@
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        if (i > 0 && arr[i] == arr[i-1]) continue;
         for (int j = i+1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 count++;
                 while (j+1 < len && arr[j] == arr[j+1]) j++;
             }
         }
+        while (i+1 < len && arr[i] == arr[i+1]) i++;
     }
     return count;
 }
@@ -20,8 +20,7 @@ int countPairs2(int *arr, int len, int value) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
             if (arr[left] == arr[right]) {
-                int n = right-left+1;
-                count += n*(n-1)/2;
+                count += (right-left+1)*(right-left)/2;
                 break;
             }
             int left_val = arr[left];
@@ -37,18 +36,15 @@ int countPairs2(int *arr, int len, int value) {
                 right_count++;
             }
             count += left_count * right_count;
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
         }
+        else if (sum < value) left++;
+        else right--;
     }
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        if (i > 0 && arr[i] == arr[i-1]) continue;
         int target = value - arr[i];
         int left = i+1;
         int right = len-1;
@@ -67,12 +63,11 @@ int countPairs3(int *arr, int len, int value) {
                     j++;
                 }
                 break;
-            } else if (arr[mid] < target) {
-                left = mid+1;
-            } else {
-                right = mid-1;
             }
+            else if (arr[mid] < target) left = mid+1;
+            else right = mid-1;
         }
+        while (i+1 < len && arr[i] == arr[i+1]) i++;
     }
     return count;
 }
