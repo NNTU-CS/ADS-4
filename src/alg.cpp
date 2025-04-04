cbb39a028328a698d93192c64a651995a9ebd0ac
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-04-04 20:12:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,25 +1,31 @@
 // Copyright 2021 NNTU-CS
 void quickSort(int* arr, int left, int right) {
-    if (left >= right) return;
-    int pivot = arr[(left + right) / 2];
-    int i = left;
-    int j = right;
-    
-    while (i <= j) {
-        while (arr[i] < pivot) i++;
-        while (arr[j] > pivot) j--;
-        if (i <= j) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+    while (left < right) {
+        int pivot = arr[(left + right) / 2];
+        int i = left;
+        int j = right;
+        while (i <= j) {
+            while (arr[i] < pivot) i++;
+            while (arr[j] > pivot) j--;
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (j - left < right - i) {
+            quickSort(arr, left, j);
+            left = i;
+        } else {
+            quickSort(arr, i, right);
+            right = j;
         }
     }
-    quickSort(arr, left, j);
-    quickSort(arr, i, right);
 }
 int countPairs1(int* arr, int len, int value) {
+    quickSort(arr, 0, len - 1);
     int count = 0;
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
@@ -46,6 +52,7 @@ int countPairs2(int* arr, int len, int value) {
             int right_val = arr[right];
             int left_count = 0;
             int right_count = 0;
+            
             while (left < len && arr[left] == left_val) {
                 left_count++;
                 left++;
@@ -70,15 +77,20 @@ int countPairs3(int* arr, int len, int value) {
         int target = value - arr[i];
         int low = i + 1;
         int high = len - 1;
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (arr[mid] == target) {
                 count++;
-                for (int j = mid - 1; j >= low && arr[j] == target; j--) {
+                int left = mid - 1;
+                while (left >= low && arr[left] == target) {
                     count++;
+                    left--;
                 }
-                for (int j = mid + 1; j <= high && arr[j] == target; j++) {
+                int right = mid + 1;
+                while (right <= high && arr[right] == target) {
                     count++;
+                    right++;
                 }
                 break;
             } else if (arr[mid] < target) {
