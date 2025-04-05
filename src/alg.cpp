c2bfe55c425f12995d2794f200153b245d1ccd9d
Krek0204 <vlad020406@yandex.ru>
2025-04-05 14:43:01 +0300

    One_two_realization.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,25 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+int countPairs1(int *arr, int len, int value) { 
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count; }
+int countPairs2(int *arr, int len, int value) { 
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = len - 1; j > len; j--) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+      if (value - arr[i] > arr[j]) {
+        break;
+      }
+    }
+  }
+  return count; }
+int countPairs3(int *arr, int len, int value) { return 0; }
