53554056714402786f92236dc37e46ffb81c39eb
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-04-05 21:47:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,74 +3,125 @@
 #include <iostream>
 #include <vector>
 
-int countPairs1(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; ++i) {
-    for (int j = i + 1; j < len; ++j) {
-      if (arr[i] + arr[j] == value) {
-        count++;
+void sortArray(int *data, int arrayLength) {
+  for (int i = 1; i < arrayLength; ++i) {
+    int currentElement = data[i];
+    int j = i - 1;
+    while (j >= 0 && data[j] > currentElement) {
+      data[j + 1] = data[j];
+      --j;
+    }
+    data[j + 1] = currentElement;
+  }
+}
+
+int countPairs1(int *inputArray, int arrayLength, int targetValue) {
+  int pairCount = 0;
+  for (int i = 0; i < arrayLength; ++i) {
+    for (int j = i + 1; j < arrayLength; ++j) {
+      if (inputArray[i] + inputArray[j] == targetValue) {
+        ++pairCount;
       }
     }
   }
-  return count;
+  return pairCount;
 }
 
-int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  int left = 0;
-  int right = len - 1;
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
-      int left_val = arr[left];
-      int right_val = arr[right];
-      int left_count = 1;
-      int right_count = 1;
+int countPairs2(int *inputArray, int arrayLength, int targetValue) {
+  int pairCount = 0;
+  int leftIndex = 0;
+  int rightIndex = arrayLength - 1;
 
-      while (left < right && arr[left + 1] == left_val) {
-        left++;
-        left_count++;
-      }
-      while (left < right && arr[right - 1] == right_val) {
-        right--;
-        right_count++;
-      }
+  while (leftIndex < rightIndex) {
+    int currentSum = inputArray[leftIndex] + inputArray[rightIndex];
 
-      count += left_count * right_count;
-      left++;
-      right--;
-    } else if (sum < value) {
-      left++;
-    } else {
-      right--;
-    }
-  }
-  return count;
-}
-
-int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len; ++i) {
-    int target = value - arr[i];
-    int left = i + 1;
-    int right = len - 1;
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] == target) {
-        int duplicate_count = 1;
-        int j = mid + 1;
-        while (j < len && arr[j] == target) {
-          duplicate_count++;
-          j++;
-        }
-        count += duplicate_count;
+    if (currentSum == targetValue) {
+      if (inputArray[leftIndex] == inputArray[rightIndex]) {
+        int numElements = rightIndex - leftIndex + 1;
+        pairCount += numElements * (numElements - 1) / 2;
         break;
-      } else if (arr[mid] < target) {
-        left = mid + 1;
       } else {
-        right = mid - 1;
+        int leftValue = inputArray[leftIndex];
+        int leftOccurrenceCount = 0;
+        while (leftIndex < arrayLength && inputArray[leftIndex] == leftValue) {
+          ++leftOccurrenceCount;
+          ++leftIndex;
+        }
+
+        int rightValue = inputArray[rightIndex];
+        int rightOccurrenceCount = 0;
+        while (rightIndex >= 0 && inputArray[rightIndex] == rightValue) {
+          ++rightOccurrenceCount;
+          --rightIndex;
+        }
+        pairCount += leftOccurrenceCount * rightOccurrenceCount;
       }
+    } else if (currentSum < targetValue) {
+      ++leftIndex;
+    } else {
+      --rightIndex;
     }
   }
-  return count;
+
+  return pairCount;
+}
+
+int findLowerBoundIndex(int *arr, int len, int target, int start) {
+  int low = start;
+  int high = len;
+  while (low < high) {
+    int middle = low + (high - low) / 2;
+    if (arr[middle] < target) {
+      low = middle + 1;
+    } else {
+      high = middle;
+    }
+  }
+  return (low < len && arr[low] == target) ? low : -1;
+}
+
+int findUpperBoundIndex(int *arr, int len, int target, int start) {
+  int low = start;
+  int high = len;
+  while (low < high) {
+    int middle = low + (high - low) / 2;
+    if (arr[middle] <= target) {
+      low = middle + 1;
+    } else {
+      high = middle;
+    }
+  }
+  return low;
+}
+
+int countPairs3(int *inputArray, int arrayLength, int targetValue) {
+  int pairCount = 0;
+
+  for (int i = 0; i < arrayLength;) {
+    int currentValue = inputArray[i];
+    int currentOccurrenceCount = 0;
+    while (i < arrayLength && inputArray[i] == currentValue) {
+      ++currentOccurrenceCount;
+      ++i;
+    }
+
+    int complementValue = targetValue - currentValue;
+
+    if (currentValue > complementValue) break;
+    if (currentValue == complementValue) {
+      pairCount += currentOccurrenceCount * (currentOccurrenceCount - 1) / 2;
+    } else {
+      int lowerIndex =
+          findLowerBoundIndex(inputArray, arrayLength, complementValue, i);
+      if (lowerIndex == -1) continue;
+
+      int upperIndex =
+          findUpperBoundIndex(inputArray, arrayLength, complementValue, i);
+      int complementOccurrenceCount = upperIndex - lowerIndex;
+
+      pairCount += currentOccurrenceCount * complementOccurrenceCount;
+    }
+  }
+
+  return pairCount;
 }
