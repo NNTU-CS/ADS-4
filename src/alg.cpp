6c81d78d94bd29ff8eb800967ca16b5b9585d363
Mariya-Norkina <128129585+Mariya-Norkina@users.noreply.github.com>
2025-04-05 20:19:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,60 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+int cnt = 0;
+for (int m = 0; m < len-1; m++) {
+for (int n = m + 1; n < len; ++n) {
+if (arr[m] + arr[n] == value) {
+cnt++;
+}
+}
+}
+return cnt;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+int cnt = 0;
+int right = len - 1;
+int left = 0;
+while (left < right) {
+int sum = arr[left] + arr[right];
+if (sum == value) {
+cnt++;
+right = skipDuplicates(arr, len, right, -1);
+left = skipDuplicates(arr, len, left, 1);
+} else if (sum < value) {
+left++;
+} else {
+right--;
+}
+}
+return cnt;
+}
+int skipDuplicates(int* arr, int len, int index, int direction) {
+int current = index;
+while (current >= 0 && current < len && arr[current] == arr[index]) {
+current += direction;
+}
+return current;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+int cnt = 0;
+for (int i = 0; i < len; ++i) {
+int target = value - arr[i];
+int left = i + 1;
+int right = len - 1;
+while (left <= right) {
+int mid = left + (right - left) / 2;
+if (arr[mid] == target) {
+cnt++;
+while (mid + 1 < len && arr[mid] == arr[mid + 1]) {
+mid++;
+}
+break; 
+} else if (arr[mid] < target) {
+left = mid + 1;
+} else {
+right = mid - 1;
+}
+}
+}
+return cnt;
 }
