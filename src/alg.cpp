ad445f3ae904c4c6f90febda3d2bcf6fdc2ccea6
Tripleez <art_khorosh@vk.com>
2025-04-05 13:47:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ void Sort(int* array, int size) {
         for (int j = 0; j < size - i - 1; ++j) {
             if (array[j] > array[j + 1]) {
                 int temp = array[j];
-                array[j] == array[j + 1];
+                array[j] = array[j + 1];
                 array[j + 1] = temp;
             }
         }
