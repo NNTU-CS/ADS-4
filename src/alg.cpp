43a33d41fe74dbb748a634e9d12d9da90ab420f0
Sergey Mescheryakov <mescheryakovsrg@gmail.com>
2025-04-05 12:59:41 +0300

    Add files via upload

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,6 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int* array, int length, int target_sum) {
+int countPairs1(const int* array, int length, int target_sum) {
   int pair_count = 0;
   for (int first = 0; first < length; ++first) {
     for (int second = first + 1; second < length; ++second) {
@@ -12,7 +12,7 @@ int countPairs1(int* array, int length, int target_sum) {
   return pair_count;
 }
 
-int countPairs2(int* array, int length, int target_sum) {
+int countPairs2(const int* array, int length, int target_sum) {
   int pair_count = 0;
   int left_index = 0;
   int right_index = length - 1;
@@ -21,23 +21,24 @@ int countPairs2(int* array, int length, int target_sum) {
     int current_sum = array[left_index] + array[right_index];
     if (current_sum == target_sum) {
       if (array[left_index] == array[right_index]) {
-        int duplicates = right_index - left_index + 1;
-        pair_count += duplicates * (duplicates - 1) / 2;
+        int dups = right_index - left_index + 1;
+        pair_count += dups * (dups - 1) / 2;
         break;
       } else {
-        int left_value = array[left_index];
-        int right_value = array[right_index];
-        int left_duplicates = 0;
+        int left_val = array[left_index];
+        int right_val = array[right_index];
+        int left_dups = 0;
+        int right_dups = 0;
 
-        while (left_index < length && array[left_index] == left_value) {
+        while (left_index < length && array[left_index] == left_val) {
           left_index++;
-          left_duplicates++;
+          left_dups++;
         }
-        while (right_index >= 0 && array[right_index] == right_value) {
+        while (right_index >= 0 && array[right_index] == right_val) {
           right_index--;
-          right_duplicates++;
+          right_dups++;
         }
-        pair_count += left_duplicates * right_duplicates;
+        pair_count += left_dups * right_dups;
       }
     } else if (current_sum < target_sum) {
       left_index++;
@@ -48,7 +49,7 @@ int countPairs2(int* array, int length, int target_sum) {
   return pair_count;
 }
 
-int binarySearchFunction(int* array, int low, int high, int key) {
+int binarySearchFun(const int* array, int low, int high, int key) {
   while (low <= high) {
     int mid = low + (high - low) / 2;
     if (array[mid] == key) {
@@ -62,11 +63,11 @@ int binarySearchFunction(int* array, int low, int high, int key) {
   return -1;
 }
 
-int countPairs3(int* array, int length, int target_sum) {
+int countPairs3(const int* array, int length, int target_sum) {
   int pair_count = 0;
   for (int i = 0; i < length; ++i) {
     int complement = target_sum - array[i];
-    int found_index = binarySearchFunction(array, i + 1, length - 1, complement);
+    int found_index = binarySearchFun(array, i + 1, length - 1, complement);
     if (found_index != -1) {
       pair_count++;
       int left = found_index - 1;
