233fb8d220e992b6d3615e017e1a4568a86f3949
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-04-05 18:43:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,90 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len; ++i) {
+        for (int j = i + 1; j < len; ++j) {
+            if (arr[i] + arr[j] == value) {
+                ++count;
+            }
+        }
+    }
+    return count;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int len, int value) {
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+
+    while (left < right) {
+        const int sum = arr[left] + arr[right];
+        if (sum < value) {
+            ++left;
+        } else if (sum > value) {
+            --right;
+        } else {
+            if (arr[left] == arr[right]) {
+                const int duplicates = right - left + 1;
+                count += duplicates * (duplicates - 1) / 2;
+                break;
+            }
+
+            int left_dup = 1;
+            while (left + left_dup < right &&
+                   arr[left + left_dup] == arr[left]) {
+                ++left_dup;
+                   }
+
+            int right_dup = 1;
+            while (left < right - right_dup &&
+                   arr[right - right_dup] == arr[right]) {
+                ++right_dup;
+                   }
+
+            count += left_dup * right_dup;
+            left += left_dup;
+            right -= right_dup;
+        }
+    }
+    return count;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+
+int countPairs3(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len - 1; ++i) {
+        const int target = value - arr[i];
+        if (target < arr[i]) continue;
+
+        int low = i + 1;
+        int high = len - 1;
+        int first = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (arr[mid] >= target) {
+                first = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (first == -1 || arr[first] != target) continue;
+
+        low = first;
+        high = len - 1;
+        int last = first;
+
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if (arr[mid] <= target) {
+                last = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        count += (last - first + 1);
+    }
+    return count;
 }
