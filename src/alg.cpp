1ef822eecacb52dccf8989c81da8acedcc2ea949
TechnoMagicc <leyzarof@gmail.com>
2025-04-05 18:55:12 +0300

    v 5.1

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,24 +11,22 @@ int countPairs1(int *arr, int len, int value) {
     return count;
 }
 int countPairs2(int *arr, int len, int value) {
-    int countPairs2(int *arr, int len, int value) {
-        int count = 0;
-        int end = len - 1;
-        while (end > 0) {
-          if (arr[end] > value) {
-            end--;
-          } else {
-            break;
-          }
-        }
-        for (int i = 0; i < len; i++) {
-          for (int j = end; j > i; j--) {
-            if (arr[i] + arr[j] == value)
-              count++;
-          }
-        }
-        return count;
+    int count = 0;
+    int end = len - 1;
+    while (end > 0) {
+      if (arr[end] > value) {
+        end--;
+      } else {
+        break;
       }
+    }
+    for (int i = 0; i < len; i++) {
+      for (int j = end; j > i; j--) {
+        if (arr[i] + arr[j] == value)
+          count++;
+      }
+    }
+    return count;
 }
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
